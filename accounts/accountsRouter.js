eab37077d1ef4122e80b46c32e4f1f075304dd7f
Megan Ennis <Megan@Megans-iMac-9.local>
2019-11-04 13:23:28 +0000

    Endpoint to add account

--- accounts/accountsRouter.js
+++ accounts/accountsRouter.js
@@ -19,11 +19,28 @@ router.get("/:id", async (req, res) => {
     const result = await db("accounts").where({ id: req.params.id });
     res.status(200).json(result[0]);
   } catch (error) {
+    res.status(500).json({
+      message: `Cannot get post with id of ${req.params.id} : ${error.message}`
+    });
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const result = await db("accounts").insert({
+      name: req.body.name,
+      budget: req.body.budget
+    });
+    res
+      .status(201)
+      .json({
+        message: "Account created successfully",
+        account: { id: result[0], name: req.body.name, budget: req.body.budget }
+      });
+  } catch (error) {
     res
       .status(500)
-      .json({
-        message: `Cannot get post with id of ${req.params.id} : ${error.message}`
-      });
+      .json({ message: "Cannot create account: " + error.message });
   }
 });
 
